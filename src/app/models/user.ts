a1b8f3d4e564168c2429e05d5eccf6ff140a800e
JonCas22 <jon.castillo@maristak.net>
2020-01-07 00:37:12 +0100

    third commit

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,10 +2,12 @@ export class User {
     nombre:string;
     email:string;
     contraseña:string;
+    isActive:boolean;
 
     constructor(nombre?:string, email?:string, contraseña?:string){
         this.nombre = nombre;
         this.email = email;
         this.contraseña = contraseña;
+        this.isActive = true;
     }
 }
--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -10,7 +10,7 @@ import { AlertController } from '@ionic/angular';
 export class UserPage implements OnInit {
 
 
-  user1:User = new User("Jon", "joncas@gmail.com", "1234");
+  user1:User = new User("Jon", "joncas@gmail.com","1234");
   user2:User = new User("Olek", "okel@gmail.com", "4567");
   user3:User = new User("Cris", "cristian@gmail.com", "8910");
   user4:User = new User("Tobi", "tobi1@gmail.com", "1112");
@@ -20,6 +20,15 @@ export class UserPage implements OnInit {
   constructor(public alertController: AlertController) {}
 
   ngOnInit() {
+  }
+
+  isActive(index){
+    if(this.users[index].isActive){
+      return "Si";
+    }else{
+      return "No";
+    }
+    
   }
 
   getUsers(){
@@ -33,6 +42,7 @@ export class UserPage implements OnInit {
   async delete(index){
     let alert = await this.alertController.create({
       header: 'Estas seguro?',
+      cssClass: 'alertPop',
       message: 'Pulsa aceptar si deseas eliminar el usuario',
       buttons: [{ text: 'Cancel', role: 'cancel' },
                 { text: 'Aceptar', handler: ()=>{
@@ -47,16 +57,32 @@ export class UserPage implements OnInit {
   async updateTask(index) {
     let alert = await this.alertController.create({
         header: 'Update Task?',
-        message: 'Type in your new task to update.',
+        cssClass: 'alertDanger',
+        message: 'Inserta los nuevos valores a actualizar',
         inputs: [{ name: 'nombre', value: this.users[index].nombre,  placeholder: 'Nombre'}, {name:'email', value: this.users[index].email, placeholder: 'Email'},
-         {name:'contraseña', value: this.users[index].contraseña,  placeholder: 'Contraseña'}],
+         {name:'contraseña', value: this.users[index].contraseña,  placeholder: 'Contraseña'}, {name: "active", type:"checkbox", checked:true, label:"Usuario Activo?"}],
         buttons: [{ text: 'Cancel', role: 'cancel' },
                   { text: 'Update', handler:  data => {
-                      this.users[index].nombre = data.nombre; this.users[index].email = data.email; this.users[index].contraseña = data.contraseña; }
+                      this.users[index].nombre = data.nombre; this.users[index].email = data.email; this.users[index].contraseña = data.contraseña; this.users[index].isActive = data.active.checked }
                   }
                  ]
     });
     await alert.present();
 }
+
+    async addUser(){
+      let alert = await this.alertController.create({
+        header: 'Añadir Usuario',
+        message: 'Introduce los datos del nuevo usuario',
+        inputs: [{ name: 'nombre',  placeholder: 'Nombre'}, {name:'email', placeholder: 'Email'},
+        {name:'contraseña', placeholder: 'Contraseña'}],
+        buttons: [{ text: 'Cancel', role: 'cancel' },
+                  { text: 'Añadir', handler:  data => {
+                      let newUser:User = new User(data.nombre, data.email, data.contraseña); this.users.push(newUser) }
+                  }
+                ]
+    });
+    await alert.present();
+    }
 
 }
